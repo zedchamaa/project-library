db7a6addafbb807c2078e6c0a4ff2be88290a820
Zed Chamaa <sales@nineprovince.com>
2022-09-30 16:06:20 +0200

    Get user input

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,8 @@
 
 // Store all books objects in an array
 let myLibrary = [];
+
+// Get user input
+const title = window.prompt('Enter book title');
+const author = window.prompt('Enter book author');
+const pages = window.prompt('Enter book pages');

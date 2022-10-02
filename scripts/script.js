e868bb58437877b4f6fa207b49978433614894a5
Zed Chamaa <sales@nineprovince.com>
2022-10-02 17:20:18 +0200

    Save library to local storage and get book from local storage

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,40 @@
 "use strict";
 
 // Store all books objects in an array
-let myLibrary = [];
+const myLibrary = [];
 
 // Get user input
-const title = window.prompt('Enter book title');
-const author = window.prompt('Enter book author');
-const pages = window.prompt('Enter book pages');
+const newBookButton = document.getElementById('new-book');
+newBookButton.addEventListener('click', () => {
+  const title = window.prompt('Enter book title');
+  const author = window.prompt('Enter book author');
+  const pages = window.prompt('Enter book pages');
+  addBookToLibrary(title, author, pages);
+})
 
-// Create a new book
+// Create a new book based on user input
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
 
-// Add book to library
-function addBookToLibrary() {
-  let i = 0;
+// Add new book to library
+function addBookToLibrary(title, author, pages) {
   const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook)[i];
-  i++;
-  console.log(myLibrary);
+  myLibrary.push(newBook);
+  saveToLocalStorage(myLibrary);
 }
 
-addBookToLibrary();
+retrieveBookFromLocalStorage();
+
+// Save library to local storage
+function saveToLocalStorage(myLibrary) {
+  localStorage.setItem('books', JSON.stringify(myLibrary));
+}
+
+// Get book from local storage
+function retrieveBookFromLocalStorage() {
+  const book = JSON.parse(localStorage.getItem('books'));
+  console.log(book.title);
+}

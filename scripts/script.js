efb56cf7aff374f7bc03992170cf218db10a487a
Zed Chamaa <sales@nineprovince.com>
2022-10-03 17:15:38 +0200

    Get user input

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,13 @@ let id = 0;
 
 // Store the index of each book in the library
 let bookIndex = 0;
+
+// Get user input
+const newBookButton = document.getElementById('new-book');
+newBookButton.addEventListener('click', () => {
+  const title = window.prompt('Enter book title');
+  const author = window.prompt('Enter book author');
+  const pages = window.prompt('Enter book pages');
+  addBookToLibrary(title, author, pages);
+  createBookCard(myLibrary);
+})

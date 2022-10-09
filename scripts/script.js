baa21f9f1425ab906844ae6fe306719709c2efcf
Zed Chamaa <sales@nineprovince.com>
2022-10-09 17:27:29 +0200

    Create a new book

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,11 @@
 
 // Store all books objects in an array
 let myLibrary = [];
+
+// Create a new book
+function Book(title, author, pages) {
+  this.id = myLibrary.length + 1;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}

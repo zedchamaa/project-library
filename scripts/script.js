b4fbc80e6be3c1f216c34eeb9a5cd0a2babbead9
Zed Chamaa <sales@nineprovince.com>
2022-10-09 17:29:04 +0200

    Create each book card and display on the web page

--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,44 @@ function addBookToLibrary(title, author, pages) {
 function saveToLocalStorage(myLibrary) {
   localStorage.setItem('library', JSON.stringify(myLibrary));
 }
+
+// Create each book card and display on the web page
+function displayBook(title, author, pages) {
+  // add a div element with a class book-card under shelves
+  const shelves = document.querySelector('.shelves');
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('book-card');
+  shelves.appendChild(bookCard);
+
+  // add the book card unique id
+  // TODO: Continue from here
+  
+  // add a div element with class book-title under book-card
+  // then push book title to it
+  const bookTitle = document.createElement('div');
+  bookTitle.classList.add('book-title');
+  bookCard.appendChild(bookTitle);
+  bookTitle.innerText = title;
+
+  // add a div element with class book-author under book-card
+  // then push book author to it
+  const bookAuthor = document.createElement('div');
+  bookAuthor.classList.add('book-author');
+  bookCard.appendChild(bookAuthor);
+  bookAuthor.innerText = author;
+
+  // add a div element with class book-author under book-card
+  // then push book author to it
+  const bookPages = document.createElement('div');
+  bookPages.classList.add('book-pages');
+  bookCard.appendChild(bookPages);
+  bookPages.innerText = pages;
+
+
+
+
+
+
+
+  
+}
